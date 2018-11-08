4793484bb6406e446467e21c38f33b531f6be421
mpbejmer <mpbejmer@gmail.com>
2018-11-08 11:41:37 +0100

    Initialize, exercise2_2

---
+++ exercis2_2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(int argc, char *argv[]){
+  int len;
+  printf("Enter length of array(integer): ");
+  scanf("%d", len);
+  printf("Entered int: %d\n", len);
+}
--- exercise1.c
+++ exercise1.c
@@ -2,5 +2,18 @@
 #include <stdlib.h>
 
 int main(){
-  
+  int m = 2;
+  int n = 5;
+  double x = 5.6;
+
+  printf("m/n = %d\n", m/n);
+  printf("v-int: x/m = %d\n", x/m);
+  printf("v-double: x/m = %lf\n", x/m);
+
+  double y = m;
+  printf("v-int: y = %d\n", y);
+  printf("v-double: y = %lf\n", y);
+
+  int k = x;
+  printf("v-int: ");
 }
